7b795f75d6bbdfbb6f3ad7826abc9fde35f6d472
Stan Stadelman <sstadelman@gmail.com>
2020-07-30 10:53:11 -0700

    specified supported platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SCNVector3Extensions",
+    platforms: [.macOS(.v10_13), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
